ff43f5ff7a8b54914474185e9fb88729323a24d8
Bryson Spilman <bryson@rmanet.com>
2022-06-20 09:04:13 -0700

    Added log for expiration buffer property

--- cumulus-client/src/main/java/mil/army/usace/hec/cumulus/client/controllers/JwtValidator.java
+++ cumulus-client/src/main/java/mil/army/usace/hec/cumulus/client/controllers/JwtValidator.java
@@ -5,12 +5,15 @@ import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import java.time.Instant;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import mil.army.usace.hec.cwms.http.client.auth.DefaultOAuth2TokenValidator;
 import mil.army.usace.hec.cwms.http.client.auth.OAuth2Token;
 import mil.army.usace.hec.cwms.http.client.auth.OAuth2TokenException;
 
 final class JwtValidator extends DefaultOAuth2TokenValidator {
 
+    private static final Logger LOGGER = Logger.getLogger(JwtValidator.class.getName());
     private static final String EXPIRATION_BUFFER_KEY = "cumulus.http.client.token.expiration.buffer.millis";
     private static final int DEFAULT_EXPIRATION_BUFFER_MILLIS = 1000;
 
@@ -35,6 +38,11 @@ final class JwtValidator extends DefaultOAuth2TokenValidator {
         String bufferMillisStr = System.getProperty(EXPIRATION_BUFFER_KEY);
         if (bufferMillisStr == null) {
             bufferMillisStr = Integer.toString(DEFAULT_EXPIRATION_BUFFER_MILLIS);
+            LOGGER.log(Level.CONFIG, () -> "Property " + EXPIRATION_BUFFER_KEY + " is not defined. Defaulting to " + DEFAULT_EXPIRATION_BUFFER_MILLIS);
+        } else {
+            String buffer = bufferMillisStr;
+            LOGGER.log(Level.CONFIG, () -> "Property " + EXPIRATION_BUFFER_KEY + " is defined. Using property value of "
+                + buffer + " as token-expiration buffer");
         }
         return Integer.parseInt(bufferMillisStr);
     }
