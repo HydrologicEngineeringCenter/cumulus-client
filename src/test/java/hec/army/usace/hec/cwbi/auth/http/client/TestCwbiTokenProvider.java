8dda6a0b69537766c225fd3e13e44aee8f0e3c78
Bryson Spilman <bryson@rmanet.com>
2022-07-08 13:56:53 -0700

    CWMS-866 Moved AuthUtil and TokenProvider class to cwbi-auth module. Moved test classes.

---
+++ src/test/java/hec/army/usace/hec/cwbi/auth/http/client/TestCwbiTokenProvider.java
@@ -0,0 +1,164 @@
+package hec.army.usace.hec.cwbi.auth.http.client;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import javax.net.ssl.SSLSocketFactory;
+import mil.army.usace.hec.cwms.htp.client.MockHttpServer;
+import mil.army.usace.hec.cwms.http.client.ApiConnectionInfo;
+import mil.army.usace.hec.cwms.http.client.ApiConnectionInfoBuilder;
+import mil.army.usace.hec.cwms.http.client.auth.OAuth2Token;
+import mil.army.usace.hec.cwms.http.client.auth.OAuth2TokenProvider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestCwbiTokenProvider {
+
+    static MockHttpServer mockHttpServer;
+
+    static ExecutorService executorService;
+
+    @BeforeAll
+    static void setUpExecutorService() {
+        executorService = Executors.newFixedThreadPool(1);
+    }
+
+    @BeforeEach
+    void setUp() throws IOException {
+        mockHttpServer = MockHttpServer.create();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        mockHttpServer.shutdown();
+    }
+
+    ApiConnectionInfo buildConnectionInfo() {
+        String baseUrl = String.format("http://localhost:%s", mockHttpServer.getPort());
+        return new ApiConnectionInfoBuilder(baseUrl).build();
+    }
+
+    protected void launchMockServerWithResource(String resource) throws IOException {
+        URL resourceUrl = getClass().getClassLoader().getResource(resource);
+        if (resourceUrl == null) {
+            throw new IOException("Failed to get resource: " + resource);
+        }
+        Path path = new File(resourceUrl.getFile()).toPath();
+        String collect = String.join("\n", Files.readAllLines(path));
+        mockHttpServer.enqueue(collect);
+        mockHttpServer.start();
+    }
+
+    private OAuth2TokenProvider getTestTokenProvider() {
+        OAuth2Token oAuth2Token = new OAuth2Token();
+        String token =
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiVXNlciIsImlzcyI6IlNpbXBsZSBTb2x1dGlvbiIsInVzZXJuYW1lIjoiVGVzdFVzZXIifQ.jQUKIOxN0KGbIGJx8SU3WfSVPNASOnRtt3DcoMVBeThcWGzEBAnwlHHYRvbzuas-sOeWSvOwrnsvpQ5tywAfWA";
+        oAuth2Token.setAccessToken(token);
+        oAuth2Token.setTokenType("Bearer");
+        oAuth2Token.setExpiresIn(3600);
+        return new OAuth2TokenProvider() {
+            @Override
+            public OAuth2Token getToken() {
+                return oAuth2Token;
+            }
+
+            @Override
+            public OAuth2Token refreshToken() {
+                return oAuth2Token;
+            }
+        };
+    }
+
+    @Test
+    void testGetToken() throws IOException {
+        String resource = "oauth2token.json";
+        launchMockServerWithResource(resource);
+        String url = buildConnectionInfo().getApiRoot();
+        CwbiAuthTokenProvider tokenProvider = new CwbiAuthTokenProvider(url, "cumulus", getTestSslSocketFactory());
+        OAuth2Token token = tokenProvider.getToken();
+        assertEquals("MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI3", token.getAccessToken());
+        assertEquals("Bearer", token.getTokenType());
+        assertEquals(3600, token.getExpiresIn());
+        assertEquals("IwOGYzYTlmM2YxOTQ5MGE3YmNmMDFkNTVk", token.getRefreshToken());
+        assertEquals("create", token.getScope());
+    }
+
+    @Test
+    void testRefreshToken() throws IOException {
+        String resource = "oauth2token.json";
+        launchMockServerWithResource(resource);
+        String url = buildConnectionInfo().getApiRoot();
+        MockCwbiAuthTokenProvider tokenProvider = new MockCwbiAuthTokenProvider(url, "cumulus", getTestSslSocketFactory());
+        OAuth2Token token = new OAuth2Token();
+        token.setAccessToken("abc123");
+        token.setTokenType("Bearer");
+        token.setExpiresIn(3600);
+        token.setRefreshToken("123abc");
+        tokenProvider.setOAuth2Token(token);
+
+        OAuth2Token refreshedToken = tokenProvider.refreshToken();
+        assertEquals("MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI3", refreshedToken.getAccessToken());
+        assertEquals("Bearer", refreshedToken.getTokenType());
+        assertEquals(3600, refreshedToken.getExpiresIn());
+        assertEquals("IwOGYzYTlmM2YxOTQ5MGE3YmNmMDFkNTVk", refreshedToken.getRefreshToken());
+        assertEquals("create", refreshedToken.getScope());
+    }
+
+    @Test
+    void testConstructor() {
+        SSLSocketFactory sslSocketFactory = getTestSslSocketFactory();
+        MockCwbiAuthTokenProvider tokenProvider = new MockCwbiAuthTokenProvider("test.com", "clientId", sslSocketFactory);
+        assertEquals("test.com", tokenProvider.getUrl());
+        assertEquals("clientId", tokenProvider.getClientId());
+        assertEquals(sslSocketFactory, tokenProvider.getSslSocketFactory());
+    }
+
+    private SSLSocketFactory getTestSslSocketFactory() {
+        return new SSLSocketFactory() {
+            @Override
+            public String[] getDefaultCipherSuites() {
+                return new String[0];
+            }
+
+            @Override
+            public String[] getSupportedCipherSuites() {
+                return new String[0];
+            }
+
+            @Override
+            public Socket createSocket(Socket socket, String s, int i, boolean b) {
+                return null;
+            }
+
+            @Override
+            public Socket createSocket(String s, int i) {
+                return null;
+            }
+
+            @Override
+            public Socket createSocket(String s, int i, InetAddress inetAddress, int i1) {
+                return null;
+            }
+
+            @Override
+            public Socket createSocket(InetAddress inetAddress, int i) {
+                return null;
+            }
+
+            @Override
+            public Socket createSocket(InetAddress inetAddress, int i, InetAddress inetAddress1, int i1) {
+                return null;
+            }
+        };
+    }
+}
