806eba50890710f5e13141b768074d58bf0ce6f7
Bryson Spilman <bryson@rmanet.com>
2022-07-21 13:48:44 -0700

    CWMS-635 Updated DownloadRequest to take in Strings for watershed and product Ids

--- cumulus-model/src/test/java/TestDownloadRequest.java
+++ cumulus-model/src/test/java/TestDownloadRequest.java
@@ -11,23 +11,19 @@ import java.util.ArrayList;
 import java.util.List;
 import mil.army.usace.hec.cumulus.client.model.CumulusObjectMapper;
 import mil.army.usace.hec.cumulus.client.model.DownloadRequest;
-import mil.army.usace.hec.cumulus.client.model.Product;
-import mil.army.usace.hec.cumulus.client.model.Watershed;
 import org.junit.jupiter.api.Test;
 
 class TestDownloadRequest {
 
     @Test
     void testSerialization() throws IOException, URISyntaxException {
-        Watershed watershed = new Watershed();
-        watershed.setId("95e7713a-ccd6-432d-b2f0-972422511171");
-        List<Product> products = new ArrayList<>();
-        Product product = new Product();
-        product.setId("74756f41-75e2-40ce-b91a-fda5aeb441fc");
-        products.add(product);
+        String watershedId = "95e7713a-ccd6-432d-b2f0-972422511171";
+        List<String> productIds = new ArrayList<>();
+        String productId = "74756f41-75e2-40ce-b91a-fda5aeb441fc";
+        productIds.add(productId);
         ZonedDateTime start = ZonedDateTime.of(2022, 4, 1, 1, 1, 1, 0, ZoneId.of("Z"));
         ZonedDateTime end = ZonedDateTime.of(2022, 4, 1, 1, 6, 1, 0, ZoneId.of("Z"));
-        DownloadRequest downloadRequest = new DownloadRequest(start, end, watershed, products);
+        DownloadRequest downloadRequest = new DownloadRequest(start, end, watershedId, productIds);
         String jsonBody = CumulusObjectMapper.mapObjectToJson(downloadRequest);
         String expectedJson = readResourceFile("cumulus/json/downloadRequest.json");
         assertEquals(expectedJson, jsonBody);
