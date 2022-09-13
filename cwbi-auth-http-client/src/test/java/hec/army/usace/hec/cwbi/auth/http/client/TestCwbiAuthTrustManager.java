81d21b581bb5dc41a825c354d1544594e1520ed0
Bryson Spilman <bryson@rmanet.com>
2022-09-13 09:43:29 -0700

    Added unit test for checkValidity() used to check for expiration.

--- cwbi-auth-http-client/src/test/java/hec/army/usace/hec/cwbi/auth/http/client/TestCwbiAuthTrustManager.java
+++ cwbi-auth-http-client/src/test/java/hec/army/usace/hec/cwbi/auth/http/client/TestCwbiAuthTrustManager.java
@@ -3,11 +3,16 @@ package hec.army.usace.hec.cwbi.auth.http.client;
 import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import hec.army.usace.hec.cwbi.auth.http.client.trustmanagers.CwbiAuthTrustManager;
+import java.security.cert.CertificateExpiredException;
 import java.security.cert.X509Certificate;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import javax.net.ssl.X509TrustManager;
 import org.junit.jupiter.api.Test;
@@ -26,6 +31,20 @@ final class TestCwbiAuthTrustManager {
         assertTrue(details.contains("CN=ISRG Root X1"));
         assertTrue(details.contains("O=Internet Security Research Group"));
         assertTrue(details.contains("C=US"));
+    }
+
+    @Test
+    void testCheckValidity() {
+        X509TrustManager trustManager = CwbiAuthTrustManager.getTrustManager();
+        assertNotNull(trustManager);
+        X509Certificate[] acceptedIssuers = trustManager.getAcceptedIssuers();
+        X509Certificate issuer = acceptedIssuers[0];
+        assertThrows(CertificateExpiredException.class, () -> issuer.checkValidity(getExpiredDate(issuer.getNotAfter())));
+    }
+
+    public static Date getExpiredDate(Date dateOfExpiration) {
+        Instant expiredInstant = dateOfExpiration.toInstant().plus(Duration.ofDays(1));
+        return Date.from(expiredInstant);
     }
 
 }
