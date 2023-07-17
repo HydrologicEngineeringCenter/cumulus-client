9b67d8e5cbd110355dbc0fb43792f6d70530a77a
Adam Korynta <adam@rmanet.com>
2023-07-17 15:13:53 -0700

    update to JDK 11 compiling for compatibility 8

--- cumulus-model/src/main/java/mil/army/usace/hec/cumulus/client/model/ProductDateCount.java
+++ cumulus-model/src/main/java/mil/army/usace/hec/cumulus/client/model/ProductDateCount.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.ZonedDateTime;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-11-02T12:49:54.974-07:00[America/Los_Angeles]")
+@javax.annotation.processing.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-11-02T12:49:54.974-07:00[America/Los_Angeles]")
 public class ProductDateCount {
 
     @JsonProperty("date")
